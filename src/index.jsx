56ff99012786987d76fba983a56e06675c05ced1
Iva Holešinská <holesinska.iva@gmail.com>
2021-05-26 22:18:53 +0200

    animals - svg

--- src/index.jsx
+++ src/index.jsx
@@ -8,18 +8,8 @@ const App = () => (
       <div className="logo"></div>
       <h1>Endemit</h1>
     </header>
-    <main>
-      <p>
-        Startovací šablona pro webovou aplikaci v Reactu. Vytvořeno pomocí{' '}
-        <a href="https://www.npmjs.com/package/create-czechitas-app">
-          create-czechitas-app
-        </a>
-        .
-      </p>
-    </main>
-    <footer>
-      <p>Czechitas, Digitální akademie: Web</p>
-    </footer>
+    <main></main>
+    <footer></footer>
   </div>
 );
 
